bb426bd8adf6eca228aff7d150a80711c53088ba
Ismail Tijani <ismailtijani10@yahoo.com>
2023-02-23 23:29:20 +0100

    Implementing user signUp controller

---
+++ src/controllers/user.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const User = require("../models/user");
+
+const signUp = async (req, res) => {
+  try {
+    const { email, firstName } = req.body;
+    const existingUser = User.findOne({ email });
+    if (existingUser)
+      res.status(422).json({
+        STATUS: "FAILURE",
+        MESSAGE: "User already exist",
+      });
+    const user = await User.create(req.body);
+    //Generate token using JWT
+    const token = await user.generateAuthToken();
+    // Using nodemailer (SMTP) for development
+    sendWelcomeEmail({
+      email,
+      subject: "Thanks for joining in!",
+      message: `Welcome to Learn More, ${firstName}. Let me know how you get along with the app`,
+    });
+    res.status(201).json({
+      STATUS: "SUCESS",
+      MESSAGE: "User created successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      STATUS: "FAILURE",
+      MESSAGE: "Internal server error",
+    });
+  }
+};
