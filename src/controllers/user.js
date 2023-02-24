44ac3c9ff553bc70984144e97210c6ee19b289e8
okworiene <okworiene6@gmail.com>
2023-02-24 01:04:49 +0100

    check coommented code

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -33,4 +33,46 @@ const signUp = async (req, res) => {
   }
 };
 
+
+
+
+
+
+
+// const router = require("express").Router()
+// const bycrypt = require("bcrypt")
+// //loading user model
+// const User = require('../../model/User')
+ 
+// // router.get('/help', (req, res) => res.json({msg:'user work'}))
+
+// // for registration of users
+// router.post('/register', (req, res) => { 
+//  User.findOne({email:req.body.email})
+//  .then(user =>{
+//     if(user){
+//         return res.status(400).json({email:"email already exist"})
+//     }else {
+//         const newUser = new User({
+//             name:req.body.name,
+//             email:req.body.email,
+//             password:req.body.password,
+//             // avatar:avatar,
+//         })
+//         bycrypt.genSalt(10,(err, salt) => { 
+//             bycrypt.hash(newUser.password, salt, (err,hash) =>{
+//                 if(err) throw err
+//                 newUser.password = hash
+//                 newUser.save()
+//                 .then(user => res .json(user))
+//                 .catch(err > console.log(err))
+//             })
+//         })
+//     }
+//  })
+// })
+// module.exports = router
+
+hello
+
 module.exports = signUp;
