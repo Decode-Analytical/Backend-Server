4c61acb6580a63fd635054231b8e0347e9ef14b4
Ismail Tijani <ismailtijani10@yahoo.com>
2023-02-24 01:00:11 +0100

    Implementing validator using joi

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const signupController = require("../controllers/user");
+const validator = require("../middleware/validator");
+const joiSchema = require("../utils/joiSchema");
 
 module.exports = (app) => {
   /*app.use('/api/routeName', yourRoute);  add new routes here
     e.g for auth
     app.use('/api/auth', authRoute);   
 */
-  app.use("user/signup", signupController);
+  app.use("user/signup", validator(joiSchema.signup, "body"), signupController);
 };
