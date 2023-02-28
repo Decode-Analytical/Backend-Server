0163c9b82a8404840adc6551cca7bdb0a32e702d
Ismail Tijani <ismailtijani10@yahoo.com>
2023-02-28 22:55:38 +0100

    Implement Authentication middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const commonService = require("../utils/commonService");
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token)
+      return commonService.unAuthorizedResponse("Please authenticate", res);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = User.findOne({ _id: decoded._id, "tokens.token": token });
+    if (!user)
+      return commonService.unAuthorizedResponse("Please authenticate", res);
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+module.exports = auth;
