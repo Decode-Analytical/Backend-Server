cc809175e9fc64a89022631e4bf93908372c17f4
Ismail Tijani <ismailtijani10@yahoo.com>
2023-03-04 22:34:10 +0100

    Adding tutor schema

--- src/models/Tutor.js
+++ src/models/Tutor.js
@@ -1,1 +1,43 @@
 // schema for tutor
+const mongoose = require("mongoose");
+
+const TutorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  subjects: [
+    {
+      type: String,
+    },
+  ],
+  profileImage: {
+    type: String,
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Tutor", TutorSchema);
