032978818979d49b917238b1648867bfe52aef30
Ismail Tijani <ismailtijani10@yahoo.com>
2023-03-04 22:04:05 +0100

    Renaming of user model repository

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const commonService = require("../utils/commonService");
 
 const auth = (req, res, next) => {
