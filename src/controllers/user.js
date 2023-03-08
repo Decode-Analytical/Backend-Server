cd6147315d0007b3baebf55fd725c3900159cb8f
Ismail Tijani <ismailtijani10@yahoo.com>
2023-03-08 10:06:31 +0100

    Implement Email sending using Nodemailer and mailtrap

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,22 @@
 const User = require("../models/user");
 const CommonService = require("../utils/commonService");
+const sendEmail = require("../emails/email.ts");
 
 const signUp = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, fullName } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return CommonService.conflictResponse("User already exist", res);
     const user = await User.create(req.body);
     //Generate token using JWT
     const token = await user.generateAuthToken();
+    //Send Welcome Email
+    sendEmail({
+      email: email,
+      subject: "Thanks for joining in!",
+      message: `Welcome to Learn More, ${fullName}. Let us know how you get along with the app`,
+    });
     CommonService.createdResponse(user, token, res);
   } catch (error) {
     CommonService.failureResponse(error.message, res);
---
+++ src/emails/email.ts
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: `<b>Hey there! </b><br> Development 😉`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, success) {
+    if (error) console.log(error);
+    console.log("Email sent: " + success.response);
+  });
+};
+
+module.exports = sendEmail;
