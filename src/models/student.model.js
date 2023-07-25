e503e64c32fb56c0784ed09d10522d71614a77f2
Faruq-Hameed <faruqhameed1@gmail.com>
2023-07-25 12:22:45 +0100

    updated

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose');
 
-const likeSchema = new mongoose.Schema({
-  courseId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-  },
-  like: {
-    type: Number,
-    enum: [0, 1, -1], //has the student like the course or not
-    required: true,
-    default: 0,
-  }
-});
-
 const studentSchema = new mongoose.Schema(
   {
     userId: {
@@ -23,7 +10,6 @@ const studentSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Course",
     },
-    registeredCourses: [likeSchema], //needed instead of using courseId bcoz a student can have more than one course
     title: {
       type: String,
     },
