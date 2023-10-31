33326effd844c5cf949d1990f9721570e855e944
Arowolo Ebine <79418698+Arosebine@users.noreply.github.com>
2023-10-31 16:36:09 +0100

    student enrollment

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -59,7 +59,7 @@ exports.studentRegisterCourse = async(req, res, next) => {
         }, {
             new: true
         });
-        return res.status(201).json({
+        return res.status(200).json({
             message: 'Course registered successfully',
             newCourse
         });
