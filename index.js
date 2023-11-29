17d838136a9c760e150c86a27ebf670d8e7ebbae
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-11-29 23:54:31 +0100

    added chat functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const { useTreblle } = require("treblle");
 const mongoSanitize = require("express-mongo-sanitize");
 const connectDB = require("./src/database/db");
@@ -17,6 +19,32 @@ const commentRoutes = require("./src/routes/comment.routes");
 const likeRoutes = require("./src/routes/like.routes");
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 connectDB();
 const port = process.env.PORT || 3000;
@@ -46,6 +74,6 @@ app.use("/api/comments/", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/quizes", questionRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Decode App is running on port, http://localhost:${port}`);
 });
