63513f8f686012038023a608615819518af989cd
Dev-Geek <confidostic3@gmail.com>
2023-12-19 14:21:25 +0100

    Load 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import { Login, Room } from "./components"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
@@ -11,10 +11,11 @@ export default function App() {
       <Provider store={store}>
         <BrowserRouter>
           <ToastContainer className="toast" />
-          <Routes>
-            <Route path="/lecture/:room" element={<Login />} />
+          <Route path="/lecture/:room" element={<Login />} />
+          {/* <Routes>
+            
             <Route path="/lecture/:room/:access" element={<Room />} />
-          </Routes>
+          </Routes> */}
         </BrowserRouter>
       </Provider>
     </>
