8fddff49e6b7872a5a27a546c15f2b7bb3703bec
Arowolo Ebine <arowolo.ebine@lampnets.com>
2024-02-22 11:46:01 +0100

    adding statistical account and payment

--- src/controllers/paystack.controller.js
+++ src/controllers/paystack.controller.js
@@ -51,12 +51,10 @@ exports.decodePaystack = async (req, res) => {
         if(event === 'charge.success'){
             const { reference } = data;
             const transaction = await Transaction.findOne({ reference });
-            console.log(transaction)
             if (!transaction) {
                 return res.status(404).json({ message: 'Transaction not found' });
             } 
-            const existingCourse = await Course.findOne({course_title: transaction.title}) 
-            console.log(existingCourse)           
+            const existingCourse = await Course.findOne({course_title: transaction.title})          
             const user = await User.findById(transaction.userId);
             if (user) {
                 await User.findOneAndUpdate(
